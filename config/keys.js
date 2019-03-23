7ab00d36a87a6b5366ce6cffc232170974dbe69c
Pro <pro@passwords-MacBook-Pro.local>
2019-03-23 13:41:51 -0700

    change database to postgres

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://test:1234@artisounds-oofih.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
+  secretOrKey: "secret",
+
+  postgresPassword: "1Q2w3e4r"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const keys = require("../../config/keys");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
+const db = require("../../config/database");
 const User = require("../../models/User");
 
 //load Input validation
@@ -17,6 +18,9 @@ const validateLoginInput = require("../../validation/login");
 //@access   public
 router.get("/test", (req, res) => {
   res.json({ msg: "users works!!!" });
+  User.findAll()
+    .then(users => console.log(users))
+    .catch(err => console.log(err));
 });
 
 //@route    POST api/users/register
@@ -30,28 +34,30 @@ router.post("/register", (req, res) => {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ email: req.body.email }).then(user => {
+  // User.findOne({ email: req.body.email }).then(user => {
+  User.findOne({ where: { email: req.body.email } }).then(user => {
     if (user) {
       errors.email = "Email already exists";
       return res.status(400).json(errors);
     } else {
+      console.log("start create");
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
         r: "pg", //Rating
         d: "mm" //Defolt
       });
-      const newUser = new User({
+      const newUser = {
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
-      });
+      };
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
           newUser.password = hash;
-          newUser
-            .save()
+          let { name, email, avatar, password } = newUser;
+          User.create({ name, email, avatar, password })
             .then(user => res.json(user))
             .catch(err => console.log(err));
         });
@@ -83,6 +89,7 @@ router.post("/login", (req, res) => {
     }
 
     //check for password
+    console.log(typeof user.password);
     bcrypt.compare(password, user.password).then(isMuch => {
       if (isMuch) {
         // user matched
