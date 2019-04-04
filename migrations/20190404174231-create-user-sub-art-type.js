39fa226094308e20c2ec4f55369bd6e65da098e3
Pro <pro@passwords-MacBook-Pro.local>
2019-04-04 11:05:32 -0700

    add migrations

---
+++ migrations/20190404174231-create-user-sub-art-type.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('UserSubArtTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      sub_art_type_id: {
+        type: Sequelize.INTEGER
+      },
+      art_type_id: {
+        type: Sequelize.INTEGER
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('UserSubArtTypes');
+  }
+};
