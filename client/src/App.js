baf73a67c2a5826dfabeed8cbe39ad678314d1c3
Yeruham <yeruham58@gmail.com>
2019-05-22 18:43:53 -0700

    add redux, register and login works with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import store from "./store";
+
 import "./App.css";
+
+// Chack for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token to get user data
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set current user
+  store.dispatch(setCurrentUser(decoded));
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>My react app</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+          </div>
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
