0f24ce870e2aca24364cf865420738117ed80c6a
Yeruham <yeruham58@gmail.com>
2019-11-26 17:17:29 -0800

    deploy test

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -176,7 +176,7 @@ class Navbar extends Component {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <Link className="nav-link" to="/register">
-            Sign Up
+            Sign-Up
           </Link>
         </li>
         <li className="nav-item">
